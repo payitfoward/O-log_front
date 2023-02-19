e59e06b5b1748d47a95d1dd7569d4165fd04c868
gaeun <geun5744@gmail.com>
2023-02-19 16:12:48 +0900

    페이지 1 로고 및 url input 완성

--- src/page1/Page1.jsx
+++ src/page1/Page1.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import logo from "./logo1.png";
+import "./page1.css";
 
 export default function Page1() {
-  return <div>페이지1</div>;
+  return (
+    <div>
+      <figure className="logo__thumb">
+        <img src={logo} alt="" className="logo" />
+      </figure>
+      <div>
+        <input
+          className="inputUrl"
+          type="text"
+          placeholder="url을 입력하세요"
+        />
+      </div>
+    </div>
+  );
 }
