e001fbec1a28e92f16fef1dcff1b6afa9f398e9c
gaeun <geun5744@gmail.com>
2023-02-19 17:03:14 +0900

    제출 버튼 완료 + 링크 삽입

--- src/page1/Page1.jsx
+++ src/page1/Page1.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "./logo1.png";
 import "./page1.css";
 
 export default function Page1() {
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // 백과 연동하는 부분
+  };
+
   return (
     <div>
       <figure className="logo__thumb">
         <img src={logo} alt="" className="logo" />
       </figure>
-      <div>
+      <form onSubmit={handleSubmit}>
         <input
           className="inputUrl"
           type="text"
           placeholder="url을 입력하세요"
+          onChange={(e) => setUrl(e.target.value)}
         />
-      </div>
+        <button type="submit" className="button">
+          <Link className="link" to="/result">
+            URL 입력
+          </Link>
+        </button>
+      </form>
     </div>
   );
 }
