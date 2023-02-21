48397809e9692db507302d4cfe85d85dae77667a
gaeun <geun5744@gmail.com>
2023-02-22 00:11:41 +0900

    1페이지 수정

---
+++ src/component/topbar/TopBar.jsx
@@ -0,0 +1,58 @@
+import "./topbar.css";
+import React from "react";
+import logo from "./logo3.png";
+
+import { Link } from "react-router-dom";
+
+export default function TopBar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i>
+          <img src={logo} alt="" className="logo2" />
+        </i>
+        <i className="topIcon fa-brands fa-square-facebook"></i>
+        <i className="topIcon fa-brands fa-square-twitter"></i>
+        <i className="topIcon fa-brands fa-square-pinterest"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
+      </div>
+
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              SERVICE
+            </Link>
+          </li>
+          <li className="topListItem"></li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/login">
+              LOGIN
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/register">
+              REGISTER
+            </Link>
+          </li>
+        </ul>
+
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
+}
